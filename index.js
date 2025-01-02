6807fa65a673d1ddf9c77966854c26d024357fd9
ThanhNga29 <103370636+ThanhNga29@users.noreply.github.com>
2025-01-02 11:19:19 +0700

    fix: stock

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 
-mongoose
-    .connect(
-        "mongodb+srv://minhhieukutecp30:9OB5TRl5sDJhL4xF@cluster0.ai0se.mongodb.net/"
-        // {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        // }
-    )
-    .then(() => console.log("Đã kết nối MongoDB"))
-    .catch((err) => console.error("Lỗi kết nối MongoDB:", err));
+// mongoose
+//     .connect(
+//         "mongodb+srv://minhhieukutecp30:9OB5TRl5sDJhL4xF@cluster0.ai0se.mongodb.net/"
+//         // {
+//         //     useNewUrlParser: true,
+//         //     useUnifiedTopology: true,
+//         // }
+//     )
+//     .then(() => console.log("Đã kết nối MongoDB"))
+//     .catch((err) => console.error("Lỗi kết nối MongoDB:", err));
 
-const inventorySchema = new mongoose.Schema({
-    product: String,
-    quantity: Number,
-});
-const Inventory = mongoose.model("Inventory", inventorySchema);
+// const inventorySchema = new mongoose.Schema({
+//     product: String,
+//     quantity: Number,
+// });
+// const Inventory = mongoose.model("Inventory", inventorySchema);
 // app.get("/input", async (req, res) => {
 //     try {
 //         const { key } = req.query;
@@ -47,7 +47,7 @@ app.get("/input", async (req, res) => {
                 'lowlevelslogan52382|pFL8OlXZqc8z|repetitious.cane@taikhoanfb.shop|csrf=8n0EwsIsOB99t6BNBuFd7wkVhYCVH5B1;rur="EAG\05467998247319\0541753325088:01f78dfbe1efa87d8e136387933b8742eb4cd98d300c5acef455661feafd9369c3947532";mid=ZqBqmwAEAAFc8XxAw87cwZpaTnpT;ds_user_id=67998247319;th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";ig_did=F499E4A0-4BFF-4267-B999-99AD7F01D4DA;sessionid=67998247319%3ArTyu6gfwaSDr3S%3A2%3AAYd8cla7qCCZVUbW08DIK3d3UQYRL8ovGIAc2NmU2A|JKTJ6YHELVNFXCXMJIS36HRXOXBR4YWF';
             return res.json([{ product: value1 }, { product: value2 }]);
         } else if (!order_id && !quantity) {
-            return res.json({ sum: 20 });
+            return res.json({ sum: 2 });
         }
     } catch (error) {
         return res
