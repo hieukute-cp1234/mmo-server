f665d48996d1a2be8fce7667e162c8e66fb6196f
mynh.hiu <apple@Mac-cua-Hiu.local>
2025-01-02 15:19:01 +0700

    feat: test api

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const PORT = 3000;
 
 // mongoose
