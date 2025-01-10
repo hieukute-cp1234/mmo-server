3f1fd2e13eac5cdbcfd0c982d1fd156bdfa88c05
ThanhNga29 <103370636+ThanhNga29@users.noreply.github.com>
2025-01-10 09:20:27 +0700

    fixbug: clg

--- index.js
+++ index.js
@@ -40,10 +40,13 @@ app.get("/input", async (req, res) => {
     try {
         const { key, order_id, quantity } = req.query;
         console.log(key);
+        console.log(order_id);
+        console.log(quantity);
         if (order_id && quantity) {
             // if (key !== "95e6173a-1bc8-40a1-95cf-7498f80a5cfc") {
             //     return res.status(403).json({ error: "Key không hợp lệ." });
             // }
+            console.log("11111111111111111111111111");
             const value1 =
                 '0lessnewsman40765|EDIit9MTO0eu|repetitious.hype@taikhoanfb.shop|csrf=2YHIHOtFAcWOMjuTjVtmQZyfjPMXkQTI;rur="CCO054681719421250541753306868:01f76fe2890a0ba7e6f020d6fa8f3497d450cdd45ad2ab98683c084909fdb66ca18659ad";mid=ZqAjbwAEAAHEZA-d9MbCLIo9eIKR;ds_user_id=68171942125;th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";th_eu_pref="";ig_did=FEF472AD-CFFF-4788-9614-9A60BC2C4875;sessionid=68171942125%3AnLSBhEQ0ArpFLQ%3A12%3AAYeyF0hTo1cQzx4XxxWjwgFLoobdx_9zqeqSUEsZkQ|CKKJKBXR4DAMMWGINAFW7MAJU7HYHFJM';
             const value2 =
@@ -53,6 +56,7 @@ app.get("/input", async (req, res) => {
             return res.json({ sum: 2 });
         }
     } catch (error) {
+        console.error(error);
         return res
             .status(500)
             .json({ error: "Lỗi khi tải tài khoản lên kho." });
